f296689af1541790c189bfa1c117c01663c4eff7
=mocnykarolek <karolek.oledzki@gmail.com>
2025-10-27 23:54:53 +0100

    main site done

--- index.js
+++ index.js
@@ -2,8 +2,13 @@
 
 // ScrollReveal().reveal('#footer', {delay: 400, origin: 'bottom', distance: '40%', reset: true});
 
-ScrollReveal().reveal("#nigga2", {
-  duration: 2000,
-  origin: "bottom",
-  distance: "300px",
-});
+// ScrollReveal().reveal("#footer", {
+//   duration: 2000,
+//   origin: "bottom",
+//   distance: "300px",
+// });
+
+
+
+
+ScrollReveal().reveal('#main_div section', { delay: 50, origin: 'left', distance: '30%', interval: 200, reset: true});
